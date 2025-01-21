a81e0c134bade0d4df7c9ccb21fb28de9d492a39
alishbakhaan <alishbakhaan649@gmail.com>
2025-01-21 17:38:08 +0500

    update

--- src/app/homesections/Articles.jsx
+++ src/app/homesections/Articles.jsx
@@ -9,8 +9,8 @@ const Articles = () => {
                 content={
                     <>
                         <div className="grid grid-cols-1 lg:grid-cols-3 items-center gap-10 p-10 lg:p-40 overflow-hidden">
-                            <div className='max-w-md lg:ml-auto'>
-                                <div className='bg-gradient-to-r from-[#ffff] h-1 w-60 max-w-20  md:mx-0 '></div>
+                            <div className='max-w-md mx-auto lg:ml-auto'>
+                            <div className='bg-gradient-to-r from-[#ffff] h-1 w-60 max-w-20 mx-auto md:mx-0 '></div>
                                 <h1 className='font-product font-bold text-white text-2xl md:text-5xl text-center md:text-left leading-normal tracking-wide pt-5'>Digital Marketing Articles</h1>
                             </div>
                             <div className="lg:col-span-2 max-w-6xl mx-auto lg:-mr-44">
